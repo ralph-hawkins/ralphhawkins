eda3717b1e514b3364a423934aec94c29f8ef18e
Ralph <ralph.hawkins1@nhs.net>
2026-07-11 21:56:51 +0100

    tidying

--- src/_11ty/filters/date-filters.js
+++ src/_11ty/filters/date-filters.js
@@ -5,15 +5,6 @@ const { DateTime } = require("luxon");
  */
 module.exports = {
   /**
-   * Format a date as dd/mm/yyyy
-   * @param {Date} date - The date to format
-   * @return {string} The formatted date string
-   */
-  shortDateFormat: function(date) {
-    return DateTime.fromJSDate(date).toFormat("dd/MM/yyyy");
-  },
-
-  /**
    * Format a date as d LLLL yyyy (e.g. 1 January 2023)
    * @param {Date} date - The date to format
    * @return {string} The formatted date string
